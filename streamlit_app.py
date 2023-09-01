7b0853708971babd8e8a3a494391eb64362f1076
olivegus <143840391+olivegus@users.noreply.github.com>
2023-09-01 16:29:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 url = "https://www.prefeitura.sp.gov.br/cidade/secretarias/fazenda/acesso_a_informacao/index.php?p=31501"
-st.write("Database is the updated [ITBI public document](%s) for transactions in Sao Paulo in 2023 " % url)
+st.write("Database is the updated [ITBI public document](%s) for transactions in Sao Paulo in 2023" % url)
